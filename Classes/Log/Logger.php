6923d0c5dbda0598ed607df8f91ab62d0904e2a3
Cloud User <ec2-user@ip-172-31-25-42.sa-east-1.compute.internal>
2017-09-19 20:30:26 -0300

    nao lembro oq mudou

--- Classes/Log/Logger.php
+++ Classes/Log/Logger.php
@@ -13,6 +13,9 @@ class Logger {
             /*if ($verbose == 2) {
                 fwrite(STDLOG, date("d-m-Y H:i:s") . " Arquivo : $file - Linha : $line - $msg\n");
             }*/
+            $date = new DateTime();
+            $log = fopen("/var/log/rv.log", "a");
+            fwrite($log, $date->format("y-m-d H:i:s")." ".$file." ".$line." "." -- ".$msg.chr(13).chr(10));
 
             fwrite(STDOUT, "VERBOSE \" '\033[01;34mArq:\033[01;32m$file - \033[01;34mL:\033[01;32m$line - $msg \033[0m'\"\n");
             fflush(STDOUT);
